cc887839756c00b925cdde33b2ed9d7e52bbe034
karrykamath <karan.kamath@hbwsl.com>
2025-04-17 11:41:01 +0530

    Added a new module named Mageplaza_HelloWorld and enabled it.

---
+++ app/code/Mageplaza/HelloWorld/Block/Index.php
@@ -0,0 +1,9 @@
+<?php
+ namespace Mageplaza\HelloWorld\Block;
+ 
+ use Magento\Framework\View\Element\Template;
+ 
+ class Index extends Template
+ {
+     // You can add custom methods here if needed
+ }
