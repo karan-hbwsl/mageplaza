b16f596353f95e6d9ff06640f90ca84636b6d9c6
karrykamath <karan.kamath@hbwsl.com>
2025-05-14 14:19:22 +0530

    Created the template for a new module named WidgetExample

---
+++ app/code/Training/WidgetExample/Controller/Index/Index.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Training\WidgetExample\Controller\Index;
+
+use Magento\Framework\App\ActionInterface;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\Controller\ResultInterface;
+
+class Index implements ActionInterface
+{
+    private PageFactory $pageFactory;
+
+    public function __construct(PageFactory $pageFactory)
+    {
+        $this->pageFactory = $pageFactory;
+    }
+
+    public function execute(): ResultInterface
+    {
+        return $this->pageFactory->create();
+    }
+}
